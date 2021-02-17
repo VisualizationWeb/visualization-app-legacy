a03a7a13cecf7d60e87ec6d1933c6697befcc282
solo5star <solo_5@naver.com>
2021-02-18 00:49:27 +0900

    Implement speech recognization (not complete yet)

--- lib/views/prefer_chat/chat_frame.dart
+++ lib/views/prefer_chat/chat_frame.dart
@@ -1,9 +1,12 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:visualization_app/models/message.dart';
 import 'package:visualization_app/widgets/chart/chart.dart';
 import 'package:visualization_app/widgets/chat_box.dart';
 import 'package:visualization_app/widgets/sliding_up.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class ChatFrame extends StatefulWidget {
   @override
@@ -26,6 +29,13 @@ class _ChatFrameState extends State<ChatFrame> {
       _history.add(message);
       _history.add(Message('결과입니다',
           additionalWidget: Chart.loadFromQuery(message.value)));
+    });
+  }
+
+  void _onListening(String message) {
+    setState(() {
+      _history.removeLast();
+      _history.add(Message(message, sendByMe: true));
     });
   }
 
@@ -54,22 +64,65 @@ class _ChatFrameState extends State<ChatFrame> {
           ),
         ]),
       ),
-      ChatInput(onSend: _sendChat)
+      ChatInput(
+        onSend: _sendChat,
+        onListening: _onListening,
+      ),
     ]);
   }
 }
 
-class ChatInput extends StatelessWidget {
-  final TextEditingController textEditingController =
-      new TextEditingController();
+class ChatInput extends StatefulWidget {
   final void Function(Message) onSend;
+  final void Function(String) onListening;
 
-  ChatInput({this.onSend});
+  ChatInput({Key key, this.onSend, this.onListening}) : super(key: key);
 
-  void _invokeSend() {
-    if (textEditingController.text.isNotEmpty) {
-      onSend(Message(textEditingController.text, sendByMe: true));
+  @override
+  _ChatInputState createState() => _ChatInputState();
+}
+
+class _ChatInputState extends State<ChatInput> {
+  final textEditingController = TextEditingController();
+
+  final stt.SpeechToText _speech = stt.SpeechToText();
+  bool _listening = false;
+  String _recognizedWords;
+
+  void _onSpeech() async {
+    final available = await _speech.initialize();
+
+    if (!available) return;
+
+    if (!_listening) {
+      log('Listening user speech ...');
+
+      _speech.listen(onResult: (result) {
+        log('User speech: ' + result.recognizedWords);
+
+        _recognizedWords = result.recognizedWords;
+
+        //widget.onListening(_recognizedWords);
+      });
+    } else {
+      log('Stopped listening');
+      log('User says: ' + _recognizedWords);
+      _speech.stop();
+
+      widget.onSend(Message(_recognizedWords, sendByMe: true));
+
+      _recognizedWords = null;
     }
+
+    setState(() {
+      _listening = !_listening;
+    });
+  }
+
+  void _onSend() {
+    if (textEditingController.text.isEmpty) return;
+
+    widget.onSend(Message(textEditingController.text, sendByMe: true));
   }
 
   @override
@@ -83,26 +136,38 @@ class ChatInput extends StatelessWidget {
         child: Row(children: [
           SizedBox(width: 16),
           Expanded(
-              child: TextField(
-                  controller: textEditingController,
-                  decoration: InputDecoration(
-                      hintText: '메세지를 입력하세요',
-                      hintStyle: TextStyle(color: Colors.white, fontSize: 20),
-                      border: InputBorder.none),
-                  onSubmitted: (value) {
-                    _invokeSend();
-                  })),
+            child: TextField(
+              controller: textEditingController,
+              decoration: InputDecoration(
+                  hintText: '메세지를 입력하세요',
+                  hintStyle: TextStyle(color: Colors.white, fontSize: 20),
+                  border: InputBorder.none),
+              onSubmitted: (value) => _onSend(),
+            ),
+          ),
           SizedBox(width: 16),
           SizedBox(
             width: 50,
             child: Container(
-                color: Color(0xFFFAB299),
-                child: IconButton(
-                  color: Colors.white,
-                  icon: Icon(Icons.send),
-                  onPressed: _invokeSend,
-                )),
-          )
+              color: _listening ? Colors.red : Colors.grey,
+              child: IconButton(
+                color: Colors.white,
+                icon: Icon(Icons.mic),
+                onPressed: _onSpeech,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 50,
+            child: Container(
+              color: Color(0xFFFAB299),
+              child: IconButton(
+                color: Colors.white,
+                icon: Icon(Icons.send),
+                onPressed: _onSend,
+              ),
+            ),
+          ),
         ]));
   }
 }
