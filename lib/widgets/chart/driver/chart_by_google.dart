79874ceb8ae31ddbb8d07554e48ee99875d48310
solo5star <solo_5@naver.com>
2021-02-18 19:15:13 +0900

    Remove unused imports

--- lib/widgets/chart/driver/chart_by_google.dart
+++ lib/widgets/chart/driver/chart_by_google.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:charts_flutter/src/text_style.dart' as chartStyle;
 import 'package:charts_flutter/src/text_element.dart' as chartText;
